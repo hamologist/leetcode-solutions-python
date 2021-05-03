42dbe8b353f4f802cd36100a7754afe92de93b17
Jordan Paris <hamologist@gmail.com>
2021-05-03 00:06:55 -0400

    BFS python implementation
      - pacific-atlantic-water-flow

--- medium/pacific-atlantic-water-flow/python/main.py
+++ medium/pacific-atlantic-water-flow/python/main.py
@@ -1,8 +1,50 @@
+from collections import deque
+from typing import Deque
+
 from _shared.python.test import BaseTest
 
 
 class Solution:
     def pacificAtlantic(self, heights: list[list[int]]) -> list[list[int]]:
+        pacific = set()
+        atlantic = set()
+        res = []
+        y_length = len(heights)
+        x_length = len(heights[0])
+
+        def bfs(_y: int, _x: int, _ocean: set):
+            queue: Deque[(int, int, int)] = deque([(_y, _x, -1)])
+
+            while queue:
+                (__y, __x, _prev) = queue.popleft()
+
+                if (__y < 0 or __x < 0 or __y >= y_length or __x >= x_length
+                        or (__y, __x) in _ocean
+                        or _prev > heights[__y][__x]):
+                    continue
+
+                _ocean.add((__y, __x))
+                queue.append((__y - 1, __x, heights[__y][__x]))
+                queue.append((__y + 1, __x, heights[__y][__x]))
+                queue.append((__y, __x - 1, heights[__y][__x]))
+                queue.append((__y, __x + 1, heights[__y][__x]))
+
+        for y in range(0, y_length):
+            bfs(y, 0, pacific)
+            bfs(y, x_length - 1, atlantic)
+
+        for x in range(0, x_length):
+            bfs(0, x, pacific)
+            bfs(y_length - 1, x, atlantic)
+
+        for y in range(0, y_length):
+            for x in range(0, x_length):
+                if (y, x) in pacific and (y, x) in atlantic:
+                    res.append([y, x])
+
+        return res
+
+    def pacificAtlanticWithDfs(self, heights: list[list[int]]) -> list[list[int]]:
         pacific = set()
         atlantic = set()
         res = []
